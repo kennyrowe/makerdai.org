c003ca6b0b61567493145cad1ec8402547c194d6
Fernando Tiberti <fertiberti@gmail.com>
2017-07-13 01:55:33 -0300

    Desktop menus

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 
+$(function() {
+  // NAV BAR menu
+  $('body').click(function(e) {
+    var menuClicked;
+    if ($(e.target).hasClass('menu-category')) {
+      menuClicked = $(e.target).parent();
+    }
+    if ($(e.target).parent().hasClass('menu-category')) { // caret clicked
+      menuClicked = $(e.target).parent().parent();
+    }
+    if (menuClicked) {
+      if (!menuClicked.hasClass('active')) {
+        $('.menu-container').removeClass('active')
+      }
+      menuClicked.toggleClass('active')
+    } else {
+      $('.menu-container').removeClass('active')
+    }
+  })
+})
